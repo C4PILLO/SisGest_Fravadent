78c40f4a62f412644fd0b0eab78e39b2f46adf1e
Luis Noblecilla <snoblecillaa@gmail.com>
2026-07-14 07:43:52 -0500

    feat: initial setup of Fravadent Spring Boot project with JPA, Security, Thymeleaf, MapStruct, and ModelMapper

---
+++ Fravadent/src/main/java/pe/com/fravadent/FravadentApplication.java
@@ -0,0 +1,13 @@
+package pe.com.fravadent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FravadentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FravadentApplication.class, args);
+	}
+
+}
---
+++ Fravadent/src/test/java/pe/com/fravadent/FravadentApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.fravadent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FravadentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
